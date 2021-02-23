400a136434d59c5b4e50d36294c0992bf293c6ad
Kandarp Kishor Jha <79475866+kandarpkishor@users.noreply.github.com>
2021-02-23 14:26:54 +0530

    Add files via upload

---
+++ connection/connection.php
@@ -0,0 +1,13 @@
+
+<?php
+$conn=mysqli_connect('localhost', 'root', '');
+$db=mysqli_select_db($conn,"school");
+$conn2=mysqli_connect('localhost', 'root', '');
+$db=mysqli_select_db($conn2,"registration");
+
+?>
+
+
+		
+		
+		
